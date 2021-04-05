7dd6e25f02ef597e2a0020edf68e6d479a7d5ff4
manhph81 <manhph81@users.noreply.github.com>
2021-04-05 14:19:51 +0700

    change HTTP status

--- controllers/user.js
+++ controllers/user.js
@@ -13,16 +13,16 @@ export const signin = async (req, res) => {
         
         const existingUser = await User.findOne({email});
         
-        if(!existingUser) return res.status(302).json({message: "User doesn't exist"})
+        if(!existingUser) return res.status(202).json({message: "User doesn't exist"})
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
-        if(!isPasswordCorrect) return res.status(302).json({message:"Invalid credentials password"})
+        if(!isPasswordCorrect) return res.status(202).json({message:"Invalid credentials password"})
         
 
         const token = jwt.sign({email:existingUser.email, id:existingUser._id}, 'test', { expiresIn:"1h" })
         res.status(200).json({result: existingUser, token})
 
     } catch (error) {
-        res.status(302).json({ message: "Something went wrong." });
+        res.status(400).json({ message: "Something went wrong." });
     }
 }
 
@@ -30,9 +30,9 @@ export const signup = async (req, res) => {
     const {email, password, firstName, lastName, confirmPassword, acType} = req.body 
     try {
         const existingUser = await User.findOne({email});
-        if(existingUser) return res.status(302).json({message: "User already exist"})
+        if(existingUser) return res.status(202).json({message: "User already exist"})
         
-        if(password!==confirmPassword) return res.status(302).json({message: "Password don't match"})
+        if(password!==confirmPassword) return res.status(202).json({message: "Password don't match"})
         if(acType!== "Supplier" && acType!== "Manufacturer" && acType!== "Distributor" && acType!== "Retailer" && acType!== "Consumer" ) return res.status(302).json({message:"Invalid Type"})
         const hashedPassword = await bcrypt.hash(password, 12)
         const alice = new driver.Ed25519Keypair()
@@ -41,7 +41,7 @@ export const signup = async (req, res) => {
 
         res.status(200).json({result, token})
     } catch (error) {
-        res.status(302).json({ message: "Something went wrong." });
+        res.status(400).json({ message: "Something went wrong." });
     }
 }
 
